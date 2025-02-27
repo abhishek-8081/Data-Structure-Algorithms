7af0d73d76dce22a3444e1ce487c4492fdc8ad43
Abhishek Kumar <132811397+abhishek-8081@users.noreply.github.com>
2025-02-27 23:54:49 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (57.64%) - LeetSync

--- 2083-three-divisors/three-divisors.cpp
+++ 2083-three-divisors/three-divisors.cpp
@@ -5,12 +5,12 @@ public:
         //     return false; 
         // }
         int cnt =0;
-        for(int i=1;i<=n;i++){
+        for(int i=1;i<=n/2;i++){
             if(n%i==0){
                 cnt++;
             }
         }
-        if(cnt==3){
+        if(cnt==2){
             return true;
         }
         else{
