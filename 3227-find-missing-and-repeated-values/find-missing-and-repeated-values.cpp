9c8b642d748ab5df515cd0fd41c22acf8c50c1bd
Abhishek Kumar <132811397+abhishek-8081@users.noreply.github.com>
2025-03-06 12:26:21 +0530

    Time: 0 ms (100.00%) | Memory: 25.5 MB (95.09%) - LeetSync

--- 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ 3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,30 +1,41 @@
 class Solution {
 public:
     vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        int n = grid.size();
-        map<int, int> mp;
-        int sum = 0, repeated = -1, missing = -1;
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                mp[grid[i][j]]++;
-                sum += grid[i][j];
+        int n = grid.size();
+
+        // n=n*n;
+        int sum=0;
+        int x=-1,y=-1;
+        long long sqsum=0;
+
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                int val = grid[i][j];
+                sum+=val;
+                sqsum+=(val*val);
             }
         }
+        n = n*n;
+        
+        int totalsum = n*(n+1)/2;
+         long long totalsqsum = (1LL*n*(n+1)*(2*n+1))/6;
+        int sumdiff= sum-totalsum;
+        long long  sqsumdiff= sqsum- totalsqsum;
+        long long  p= sqsumdiff / sumdiff;
+         x= (p+sumdiff)/2;
+         y= (p-x);
 
-        int totalSum = (n * n * (n * n + 1)) / 2;  
+         return {x,y};
+
+
+
+
 
         
-        for (auto it : mp) {
-            if (it.second == 2) {  
-                repeated = it.first;
-                break;
-            }
-        }
 
-       
-        missing = totalSum - (sum - repeated);
 
-        return {repeated, missing};
+        
     }
 };
