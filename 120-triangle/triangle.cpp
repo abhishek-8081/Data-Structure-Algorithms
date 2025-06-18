d4a2ed578b1183000b2d4ea22d909a4fa345ae7d
Abhishek Kumar <132811397+abhishek-8081@users.noreply.github.com>
2025-06-18 15:03:04 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (51.52%) - LeetSync

--- 120-triangle/triangle.cpp
+++ 120-triangle/triangle.cpp
@@ -1,21 +1,19 @@
 class Solution {
-    private:
-    int f(int i,int j,int n, vector<vector<int>>&matrix,vector<vector<int>>&dp){
-        if(i==n-1){
-            return matrix[n-1][j];
-        }
-        if(dp[i][j]!=1e9) return dp[i][j];
-        int down = matrix[i][j]+f(i+1,j,n,matrix,dp);
-        int rd=matrix[i][j]+f(i+1,j+1,n,matrix,dp);
-        return dp[i][j]=min(down,rd);
-
-
-    }
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
         vector<vector<int>>dp(n,vector<int>(n,1e9));
-        return f(0,0,n,triangle,dp);
+        for(int j=0;j<n;j++){
+            dp[n-1][j]=triangle[n-1][j];
+        }
+        for(int i=n-2;i>=0;i--){
+            for(int j=0;j<=i;j++){
+                int down=triangle[i][j]+dp[i+1][j];
+                int diagonal = triangle[i][j]+dp[i+1][j+1];
+                dp[i][j]=min(down,diagonal);
+            }
+        }
+        return dp[0][0];
 
     }
 };
