7eb6829b3d7951db3e6cd431a083850f7a5021e2
Abhishek Kumar <132811397+abhishek-8081@users.noreply.github.com>
2025-10-08 09:12:34 +0530

    Time: 3 ms (5.73%) | Memory: 10.7 MB (22.52%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,33 +1,22 @@
 class Solution {
-    private:
-     int maximumSum(vector<int>&nums){
-        int n = nums.size();
-        int prev = nums[0];
-        int prev2=0;
-        for(int i=1;i<n;i++){
-            int take = nums[i];
-            if(i>1)take+=prev2;
-            int nottake= 0+prev;
-            int curr = max(take,nottake);
-            prev2=prev;
-            prev= curr;
-        }
-        return prev;
+    int solve(vector<int>&nums){
+     int n = nums.size();
+    vector<int>dp(n+3,0);
+   
+    // dp[n]=0;
+
+    for(int i=n-1;i>=0;i--){
+        int include = nums[i]+dp[i+2];
+        int exclude = 0+dp[i+1];
+        dp[i] =max(include,exclude);
+
     }
+    return dp[0];
+
+}
 public:
-    int rob(vector<int>& money) {
-         int n = money.size();vector<int>temp1,temp2;
-        if(n==1) return money[0];
-        // for(int i=0;i<n;i++){
-        //     if(i!=0)temp1.push_back(money[i]);
-        //     if(i!=n-1)temp2.push_back(money[i]);
-           
-
-        // }
-        
-        return maximumSum(money);
-
+    int rob(vector<int>& nums) {
+        return solve(nums);
         
     }
 };
-
